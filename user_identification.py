52da9194f9992eb13aa615e49f687ecf1cf33f7b
Hraban Luyat <hraban@0brg.net>
2007-02-01 08:11:25 +0000

    fixed some bugs in user_identification.py

--- user_identification.py
+++ user_identification.py
@@ -1,34 +1,36 @@
-'''functions that allow manipulation of the database of unique user identities. these functions include things like fetching the name corresponding to a certain id or vice-versa, adding new identities, etc.'''
+'''functions that allow manipulation of the database of unique user identities.
+these functions include things like fetching the name corresponding to a certain
+id or vice-versa, adding new identities, etc.'''
 
 #TODO: a good function is needed to get a UID from a string. one that,
 #for example, correctly handles xmpp:node@domain/resource
+#TODO: this module too, needs a lot of cleaning.
 
 import mysql
 import stringfilters as filters
 
 def addUid(name,typ):
-	'''add a new uid to the database. the typ argument is passed
-directly to the function for checking types, the name is inserted
-in the database. Do note that if self.getTypeId() raises an exception,
-it will not be catched. If the uid already exists it will return its
-uid.
+	'''add a new uid to the database. the typ argument is passed directly to the
+	function for checking types, the name is inserted in the database. Do note
+	that if self.getTypeId() raises an exception, it will not be catched. If the
+	uid already exists it will return its uid.
 
-fixme; this function is pretty ugly. addUid() shouldn't return the
-uid of an already existing entry. also, it shouldn't use try:
-except: statements if it's going to check for it anyway. (only
-use try: except: if you don't expect something to happen!)'''
+	fixme; this function is pretty ugly. addUid() shouldn't return the uid of an
+	already existing entry. also, it shouldn't use try: except: statements if it's
+	going to check for it anyway. (only use try: except: if you don't expect
+	something to happen!)'''
 	try:
-		return getId(name,typ)
+		return getId(name, typ)
 	except ValueError: #if new, then continue
 		try:
-			typ=int(typ)
+			typ = int(typ)
 		except ValueError:
-			typ=getTypeId(typ)
-		cursor=mysql.db_w.cursor()
+			typ = getTypeId(typ)
+		cursor = mysql.db_w.cursor()
 		try:
-			cursor.execute("insert into `uids`(`name`,`type`) values(%s,%s)",(name,typ))
-		except mysql.ProgrammingError, e:
-			print "\n\n  >> DATABASE ERROR: QUERY FAILED <<\n\n  >> %s\n\n"%e.__str__()
+			cursor.execute("insert into `uids`(`name`,`type`) values(%s,%s)", (name, typ))
+		except cursor.ProgrammingError, e:
+			print "\n\n  >> DATABASE ERROR: QUERY FAILED <<\n\n  >> %s\n\n" % e
 			cursor.close()
 			return "database error: query failzored :("
 		else:
@@ -36,70 +38,73 @@ use try: except: if you don't expect something to happen!)'''
 			return getId(name,typ)
 
 
-def getId(name,typ):
+def getId(name, typ):
 	'''fetch the uid of the user with name name (str) and of type typ (str). return as an integer.'''
-	typ=getTypeId(typ)
-	cursor=mysql.db_r.cursor()
+	typ = getTypeId(typ)
+	cursor = mysql.db_r.cursor()
 	try:
-		n=cursor.execute("select `id` from `uids` where `name`=%s and `type`=%s limit 1",(name,typ)) #.execute() returns the number of rows
-	except mysql.ProgrammingError, e:
-		print "\n\n  >> DATABASE ERROR: QUERY FAILED <<\n\n  >> %s\n\n"%e.__str__()
+		n = cursor.execute("select `id` from `uids` where `name`=%s and `type`=%s limit 1", (name, typ))
+		#.execute() returns the number of rows
+	except cursor.ProgrammingError, e:
+		print "\n\n  >> DATABASE ERROR: QUERY FAILED <<\n\n  >> %s\n\n" % e
 		cursor.close()
 		return 0 #an integer is expected
 	else:
 		if not n:
 			cursor.close()
 			raise ValueError, "user not found"
-		result=cursor.fetchone()
+		result = cursor.fetchone()
 		cursor.close()
 		return int(result[0])
 
-def getIdNoBullShit(name,typ):
+def getIdNoBullShit(name, typ):
 	'''same as getId() but make a new one if it doesn't exist yet'''
 	try:
-		return getId(name,typ)
+		return getId(name, typ)
 	except ValueError:
-		return addUid(name,typ)
+		return addUid(name, typ)
 
 
 
 def getName(uid):
 	'''fetch the row with id = uid (int) from the database and return the name of the user (str)'''
-	uid=int(uid) #fixme: not sure, should we check for exception or not?
-	query="select `name` from `uids` where `id` = '%d' limit 1;"%uid
-	cursor=mysql.db_r.cursor()
-	n=cursor.execute(query)
+	uid = int(uid) #fixme: not sure, should we check for exception or not?
+	query="select `name` from `uids` where `id` = %d limit 1;" % uid
+	cursor = mysql.db_r.cursor()
+	n = cursor.execute(query)
 	if not n:
 		cursor.close()
 		raise ValueError, "user not found"
-	result=cursor.fetchone()
+	result = cursor.fetchone()
 	cursor.close()
 	return result[0]
 
 def getType(typeid):
-	'''returns the conversation type with the specified id (int||str). note that int(id) is executed without for an exception.'''
-	typeid=int(typeid)
-	query="select `type` from `convotypes` where `id` = '%s' limit 1;"
-	cursor=mysql.db_r.cursor()
-	n=cursor.execute(query)
+	'''returns the conversation type with the specified id (int||str). note that
+	int(id) is executed without for an exception.'''
+	query = "select `type` from `convotypes` where `id` = %s limit 1;"
+	cursor = mysql.db_r.cursor()
+	n = cursor.execute(query, typeid)
 	if not n:
 		cursor.close()
 		raise ValueError, "conversation type not found"
-	result=cursor.fetchone()
+	result = cursor.fetchone()
 	cursor.close()
 	return result[0]
 
 def getTypeId(typ):
-	'''returns the id (int) of the supplied typ(str), fetched from the database that holds the different conversation types. the ValueError exception is raised if it is not an existant conversation type.'''
+	'''returns the id (int) of the supplied typ(str), fetched from the database
+	that holds the different conversation types. the ValueError exception is raised
+	if it is not an existant conversation type.'''
 	try: #maybe it's already an integer...
 		return int(typ)
 	except ValueError:
-		cursor=mysql.db_r.cursor()
-		n=cursor.execute("select `id` from `convotypes` where `type`=%s limit 1",(typ,))
+		cursor = mysql.db_r.cursor()
+		n = cursor.execute("select `id` from `convotypes` where `type`=%s limit 1", (typ,))
 		if not n:
 			cursor.close()
 			raise ValueError, "conversation type not found"
-		result=cursor.fetchone()
+		result = cursor.fetchone()
 		cursor.close()
 		return int(result[0])
 
@@ -107,7 +112,8 @@ def getTypeId(typ):
 
 def getPermissions(uid):
 	'''return an iterable object that holds all the permissions a uid has
-#fixme: currently, this just returns 'all' if the uid matches the jid of an admin, hardcoded here. obviously, we need a real system for this.'''
+	TODO: currently, this just returns 'all' if the uid matches the jid of an
+	admin, hardcoded here. obviously, we need a real system for this.'''
 	if int(uid) == 6:
 		return ( 'all', )
 	else:
@@ -116,7 +122,7 @@ def getPermissions(uid):
 
 def isAllowedTo( uid, whatdoeshewanttodothen ):
 	'''check if uid (int) is allowed to do whatdoeshewanttodothen (str).
-returns True (bool) if allowed, False otherwise.'''
+	returns True (bool) if allowed, False otherwise.'''
 	permissions = getPermissions( uid )
 	if 'all' in permissions:
 		return True
