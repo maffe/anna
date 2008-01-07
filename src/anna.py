d0b2ee7563d9605e26bb0505235e5c1582127cf3
Hraban Luyat <hraban@0brg.net>
2008-01-07 05:56:43 +0000

    Modifications to the startup script.

--- src/anna.py
+++ src/anna.py
@@ -8,31 +8,36 @@ import frontends.console
 import frontends.xmpp
 
 def handle_sig(sign, frame):
-    cleanup()
-    sys.exit()
+    """Debug signal handler."""
+    print >> sys.stderr, "SIGNAL: %r: %r" % (sign, frame)    
 
-def init():
-    global pool
-    pool = []
-    signal.signal(signal.SIGINT, handle_sig) # DEBUG
-    c.start()
-    pool.append(frontends.console.Connection())
-    pool.append(frontends.xmpp.Connection())
+class Anna(object):
+    """The Anna bot.
 
-def run():
-    """This is the phase that the main thread is in until all threads die."""
-    for thread in pool:
-        thread.start()
-    for thread in pool:
-        thread.join()
+    Use the L{start} method to start the bot.
 
-def cleanup():
-    c.stop()
+    @TODO: Handle SIGINT properly (immediately).
+
+    """
+    def __init__(self):
+        signal.signal(signal.SIGINT, handle_sig)
+        pool = []
+        #pool.append(frontends.console.Connection())
+        pool.append(frontends.xmpp.Connection())
+        self.pool = pool
+
+    def run(self):
+        """This is where the main thread is until all frontend threads die."""
+        for thread in self.pool:
+            thread.start()
+        for thread in self.pool:
+            thread.join()
 
 def main():
-    init()
-    run()
-    cleanup()
+    c.start()
+    bot = Anna()
+    bot.run()
+    c.stop()
 
 if __name__ == "__main__":
     main()
