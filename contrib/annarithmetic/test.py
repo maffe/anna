a287bf174e055a37558eef5119898f2a91015108
Hraban Luyat <hraban@0brg.net>
2008-02-02 13:49:38 +0000

    Improvements to the unit tester for the annarithmetic plugin.

--- contrib/annarithmetic/test.py
+++ contrib/annarithmetic/test.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
-"""Module used for testing the annarithmetic plugin.
+"""Module used for testing the annarithmetic plugin."""
 
-@TODO: Interpret ` as a wild-card character.
-
-"""
-USAGE = """
-Usage:
-    $ test.py TEST_FILE
-
-"""
 import csv
 import os
+import re
 import sys
 
 import jpype as j
+
+USAGE = """
+Usage:
+    $ %s TEST_FILE
+
+""" % sys.argv[0]
 
 def start_JVM_from_curdir():
     path = os.path.join(os.path.abspath(os.curdir))
@@ -27,10 +26,14 @@ def test(testfile):
     passed = 0
     # OK, this is ugly, but I don't know a better way to get pretty output.
     prev_nl = False
-    for test, sol in tests:
+    for test, solu in tests:
         i += 1
+        # Create a compiled regular expression from this test. Changes "0.6`7"
+        # into r"0\.6\d*7$". I.e.: "`" means "any number of digits".
+        rsolu = "%s$" % re.escape(solu).replace(r"\`", r"\d*")
         result = p.processMessage(test)
-        if result == sol:
+        # .match() matches at the start of the string.
+        if re.match(rsolu, result) is not None:
             sys.stdout.write("\rRunning tests... [%d/%d] OK" % (i, len(tests)))
             sys.stdout.flush()
             passed += 1
@@ -39,16 +42,17 @@ def test(testfile):
             if not prev_nl:
                 print
             print "FAIL: [%d/%d]:" % (i, len(tests)), repr(test), "returns",
-            print "'%s', should be: '%s'" % (result, sol)
+            print "'%s', should be: '%s'" % (result, solu)
             prev_nl = True
     print "\rTest results:", passed, "of", len(tests), "passed."
 
 def main():
     print __doc__
     try:
-        test(open(sys.argv[1]))
+        filename = sys.argv[1]
     except IndexError:
         print >> sys.stderr, USAGE
+    test(open(filename))
 
 if not j.isJVMStarted():
     start_JVM_from_curdir()
