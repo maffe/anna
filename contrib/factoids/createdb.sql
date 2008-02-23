a8d90ac4f1aaa0a9e5c81986720869746d0be754
Hraban Luyat <hraban@0brg.net>
2008-02-23 23:34:57 +0000

    Factoids plugin's createdb.sql file no longer really needed.

--- contrib/factoids/createdb.sql
+++ contrib/factoids/createdb.sql
@@ -1,3 +1,6 @@
+-- This file only describes the lay-out of the table, it is not needed (and not
+-- used) for creating it. That is done by the factoids plugin automatically.
+-- This SQL statement, by the way, was created specifically for Sqlite3.
 CREATE TABLE factoid (
 	id INTEGER PRIMARY KEY,
 	numreq INTEGER NOT NULL DEFAULT 0,
