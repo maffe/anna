2c26930e9a432b54e1f5744a02ccc31aa9a4336d
Hraban Luyat <hraban@0brg.net>
2008-06-13 23:04:51 +0000

    Use urllib instead of httplib for annai dump plugin.

--- src/ai/annai/plugins/dump.py
+++ src/ai/annai/plugins/dump.py
@@ -3,12 +3,12 @@
 See U{https://0brg.net/anna/wiki/Dump_plugin} for more information.
 
 """
-import httplib
 import re
 try:
     import thread as _thread
 except ImportError:
     import dummy_thread as _thread
+import urllib
 
 from ai.annai.plugins import BasePlugin
 import communication as c
@@ -23,14 +23,11 @@ _fetch_lock = _thread.allocate_lock()
 def _get_dump(party, id):
     """Get a dump with given id and send the contents to this peer."""
     _fetch_lock.acquire()
-    h = httplib.HTTPConnection("b4.xs4all.nl")
-    h.request("GET", "/dump/%s.txt" % id)
-    response = h.getresponse()
-    if response.status == 200:
-        encoding = response.getheader("content-type").split("charset=")[1]
-        content = response.read().decode(encoding, "replace").strip()
-        party.send(u"Dump #%s:\n\n%s" % (id, content))
-    h.close()
+    u = urllib.urlopen("http://b4.xs4all.nl/dump/%s.txt" % id)
+    encoding = u.info().getheader("content-type").split("charset=")[1]
+    content = u.read().decode(encoding, "replace").strip()
+    party.send(u"Dump #%s:\n\n%s" % (id, content))
+    u.close()
     _fetch_lock.release()
 
 class _Plugin(BasePlugin):
