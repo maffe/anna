613d538f8ea295e3aa5b837cb74736f466641228
Hraban Luyat <hraban@0brg.net>
2008-08-08 21:56:03 +0000

    New plugins in contrib: - DOS line endings (CRLF) changed to UNIX line endings (LF). - Tabs changed to four spaces. - Crashes fixed.

--- contrib/repeat-repeated-k.py
+++ contrib/repeat-repeated-k.py
@@ -3,23 +3,23 @@
 from ai.annai.plugins import BasePlugin
 
 class _Plugin(BasePlugin):
-	self.sent = False
-	self.wask = False
+    sent = False
+    wask = False
 
-	def __unicode__(self):
-		return u"repeat k plugin"
+    def __unicode__(self):
+        return u"repeat k plugin"
 
-	def process(self, message, reply, *args):
-		if message == u"k":
-			if self.wask:
-				if not self.sent:
-					self.sent = True
-					return (message, message)
-			self.wask = True
-		else:
-			self.sent = False
-			self.wask = False
-		return (message, reply)
+    def process(self, message, reply, *args):
+        if message == u"k":
+            if self.wask:
+                if not self.sent:
+                    self.sent = True
+                    return (message, message)
+            self.wask = True
+        else:
+            self.sent = False
+            self.wask = False
+        return (message, reply)
 
 OneOnOnePlugin = _Plugin
 ManyOnManyPlugin = _Plugin
--- contrib/repeat-repeated-message.py
+++ contrib/repeat-repeated-message.py
@@ -3,21 +3,21 @@
 from ai.annai.plugins import BasePlugin
 
 class _Plugin(BasePlugin):
-	self.sent = False
-	self.old = u""
+    sent = False
+    old = u""
 
-	def __unicode__(self):
-		return u"repeat message plugin"
+    def __unicode__(self):
+        return u"repeat message plugin"
 
-	def process(self, message, reply, *args):
-		if message == self.old:
-			if not self.sent:
-				self.sent = True
-				return (message, message)
-		else:
-			self.old = message
-			self.sent = False
-		return (message, reply)
+    def process(self, message, reply, *args):
+        if message == self.old:
+            if not self.sent:
+                self.sent = True
+                return (message, message)
+        else:
+            self.old = message
+            self.sent = False
+        return (message, reply)
 
 OneOnOnePlugin = _Plugin
 ManyOnManyPlugin = _Plugin
