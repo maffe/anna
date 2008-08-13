cc1e58781b5b08ceeaddf744c66f5dc19e9f5a97
Hraban Luyat <hraban@0brg.net>
2008-08-13 07:20:47 +0000

    IRC frontend improved (existing participants are now recognized, not just participants that joined after the bot did).

--- contrib/ircfrontend/connection.py
+++ contrib/ircfrontend/connection.py
@@ -32,6 +32,16 @@ _conf = config.get_conf_copy()
 _def_ai_mom = aihandler.get_manyonmany(_conf.misc["default_ai"])
 _def_ai_ooo = aihandler.get_oneonone(_conf.misc["default_ai"])
 
+def _irc_nameq(x, y):
+    """Returns True if two names are considered equal according to the RFC."""
+    # Better to call this with unicode objects, of course, but if it is sure
+    # that the encoding is ASCII compatible (IRC nicknames MUST contain only
+    # ACII chars) you can call it with byte strings too.
+    assert(isinstance(x, basestring))
+    assert(isinstance(y, basestring))
+    assert(type(x) == type(y))
+    return irclib.irc_lower(x) == irclib.irc_lower(y)
+
 class _ServerBot(ircbot.SingleServerIRCBot):
     def __init__(self, *args, **kwargs):
         ircbot.SingleServerIRCBot.__init__(self, *args, **kwargs)
@@ -41,17 +51,46 @@ class _ServerBot(ircbot.SingleServerIRCBot):
     def late_on_join(self, conn, ev):
         """Just like an on_join handler but with higher priority number.
 
-        This handler must be called after C{SingleServerIRCBot}'s built-in
-        on_join handler.
+        This handler must be called after C{ircbot.SingleServerIRCBot}'s
+        built-in on_join handler.
 
         """
-        if irclib.nm_to_n(ev.source()) == conn.get_nickname():
+        if _irc_nameq(irclib.nm_to_n(ev.source()), conn.get_nickname()):
             # This event was triggered because this bot just joined.
             chan_name_enc = ev.target()
             chan_name = chan_name_enc.decode(ENC)
-            chan = Group(conn, name=chan_name, encoding=ENC)
-            chan.set_AI(_def_ai_mom(chan))
-            self.channels[chan_name_enc]._annagroup = chan
+            group = Group(conn, name=chan_name, encoding=ENC)
+            group.set_AI(_def_ai_mom(group))
+            self.channels[chan_name_enc]._annagroup = group
+
+    def on_namreply(self, conn, ev):
+        names = map(lambda x: (x, x.decode(ENC)), ev.arguments()[2].split())
+        for name_enc, name in names:
+            if _irc_nameq(name_enc, conn.get_nickname()):
+               continue 
+            group = self.channels[ev.arguments()[1]]._annagroup
+            # Hack: the API doesn't describe a way to check if a participant is
+            # already in a room, so the exception means it is not.
+            try:
+                group.get_participant(name)
+            except NoSuchParticipantError:
+                group.add_participant(GroupMember(name,
+                        self.connection.get_server_name().decode(ENC)))
+
+    def on_nick(self, conn, ev):
+        from_enc = irclib.nm_to_n(ev.source())
+        from_ = from_enc.decode(ENC)
+        to_enc = ev.target()
+        to = to_enc.decode(ENC)
+        if _irc_nameq(to_enc, conn.get_nickname()):
+            # Ignore incoming NICKs indicating nickchange of the bot itself.
+            return
+        for group in (chan._annagroup for chan in self.channels):
+            try:
+                gmem = group.get_participant(from_)
+                gmem.nick = to
+            except NoSuchParticipantError:
+                pass
 
     def on_nicknameinuse(self, conn, ev):
         conn.nick(conn.get_nickname() + "_")
@@ -74,14 +113,14 @@ class _ServerBot(ircbot.SingleServerIRCBot):
         chan_name = ev.target().decode(ENC)
         c.stderr(u"DEBUG: irc: PRIVMSG (pub) from %s: %s\n" % (sender_name,
             msg))
-        chan = self.channels[chan_name_enc]._annagroup
+        group = self.channels[chan_name_enc]._annagroup
         try:
-            part = chan.get_participant(sender_name)
+            part = group.get_participant(sender_name)
         except NoSuchParticipantError, e:
-            part = GroupMember(nick,
+            part = GroupMember(sender_name,
                     self.connection.get_server_name().decode(ENC))
-            chan.add_participant(part)
-        chan.get_AI().handle(msg, part)
+            group.add_participant(part)
+        group.get_AI().handle(msg, part)
 
     def on_welcome(self, conn, ev):
         try:
