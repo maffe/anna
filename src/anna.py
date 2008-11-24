6e5f300c4305f69a92b478679ff450eecd460e47
Hraban Luyat <hraban@0brg.net>
2008-11-24 02:58:46 +0000

    Main anna.py script (should) handle(s) keyboard interrupt signal (ctrl+c) better. Also only registers ctrl+c handler when run stand-alone.

--- src/anna.py
+++ src/anna.py
@@ -8,7 +8,6 @@ To kill the entire chatbot, use ctrl + C.
 import logging
 import optparse
 import os
-import signal
 import sys
 try:
     import threading as _threading
@@ -62,7 +61,6 @@ class Anna(object):
 
     """
     def __init__(self, frontends):
-        signal.signal(signal.SIGINT, stop)
         self.pool = []
         for name in frontends:
             self.pool.append(_frontends[name].Connection())
@@ -106,8 +104,7 @@ def stop():
         bot.stop()
     else:
         _logger.warning(u"Not waiting for threads to finish.")
-        # Forcefully flush thread reference pool.
-        bot.pool = []
+        sys.exit("Interrupted.")
 
 def modify_syspath():
     """Include the necessary dirs in C{sys.path} for dependencies."""
@@ -137,9 +134,13 @@ def main():
         p.error("You need to specify at least one frontend. See --list.")
     logging.basicConfig(level=options.loglevel, format=LOGFORMAT,
             datefmt=LOGDATEFMT)
+    # Temporary: filter out all non-anna debug messages.
+    logging.root.handlers[0].addFilter(logging.Filter('anna'))
     config.load_conf(options.file)
     c.start()
     _import_frontends(args)
+    import signal
+    signal.signal(signal.SIGINT, stop)
     bot = Anna(args)
     bot.start()
     # time.sleep() is interrupted by signals, unlike threading.Thread.join().
